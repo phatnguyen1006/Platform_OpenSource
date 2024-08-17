4ec9784a70034fefe6c0c1491fc3f4f0cffa258a
P <69576826+phatnguyen1006@users.noreply.github.com>
2024-08-17 23:51:48 +0700

    Update endian.swift

--- swift/endian.swift
+++ swift/endian.swift
@@ -6,7 +6,15 @@ import Foundation
 import CoreFoundation
 
 let integer = 32
+
+// MARK: Converts a 16-bit integer from big-endian format to the host’s native byte order.
 CFSwapInt32BigToHost(arg: UInt32(bigEndian: integer))
+
+// MARK: Converts a 16-bit integer from the host’s native byte order to big-endian format.
 CFSwapInt32HostToBig(arg: UInt32(integer))
-CFSwapInt32HostToLittle(arg: UInt32(littleEndian: integer))
+
+// MARK: Converts a 16-bit integer from little-endian format to the host’s native byte order.
+CFSwapInt16LittleToHost(arg: UInt32(littleEndian: integer))
+
+// MARK: Converts a 16-bit integer from the host’s native byte order to littile-endian format.
 CFSwapInt32HostToLittle(arg: UInt32(integer))
